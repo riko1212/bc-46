58aab2d0f3ea99f8dfe15fa8a28c0327e9338254
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2022-12-08 12:55:52 +0200

    mod-6

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,21 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    radioList: document.querySelectorAll('.radio'),
+    btn: document.querySelector('.button'),
   };
+  let temp = 0;
+  for (item of refs.radioList) {
+    if (item.checked) {
+      temp += 1;
+    }
+  }
+  if (!temp) {
+    // refs.btn.setAttribute('disabled');
+    console.log(1);
+  } else {
+    console.log(2);
+  }
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
